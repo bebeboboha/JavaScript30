c6f5e1047e747bdb95b2e54111e22af6b7f6fae4
Peggy <bebebobohaha@gmail.com>
2017-03-07 16:53:03 +0800

    volume rate

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -18,10 +18,22 @@ function togglePlay(){
 }
 
 function updateButton(){
-	console.log('change')
+	const icon = this.paused? '►' : '❚❚'
+	toggle.textContent = icon
+}
+
+function skip(){
+	video.currentTime += parseFloat(this.dataset.skip)
+}
+
+function handleRangeUpdate(){
+	video[this.name] = this.value
 }
 
 video.addEventListener('click',togglePlay)
 video.addEventListener('play',updateButton)
 video.addEventListener('pause',updateButton)
 toggle.addEventListener('click',togglePlay)
+skipButtons.forEach(button => button.addEventListener('click',skip))
+ranges.forEach(range => range.addEventListener('change',handleRangeUpdate))
+ranges.forEach(range => range.addEventListener('mousemove',handleRangeUpdate))
